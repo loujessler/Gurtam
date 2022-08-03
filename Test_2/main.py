842d8ffb85d7fad12e5c674279c17909116f09c1
loujessler <danilin055@yahoo.com>
2022-08-03 14:17:14 +0400

    Корректировка второго задания

--- Test_2/main.py
+++ Test_2/main.py
@@ -1,26 +1,31 @@
-# Инверсия двоичного числа
-def reverse_bit(bit):
-    return ''.join('0' if int(bit[x]) else '1' for x in range(0, len(bit)))
+
+def rev_bits(x):
+    rev = 0
+    while x:
+        rev <<= 1
+        rev += x & 1
+        x >>= 1
+    return rev
 
 
-# Добавление недостоющего 0
-def add_zero(bit):
-    if len(bit) % 2 is 1:
-        bit = '0' + bit
-    return bit
+def main2(msg):
+    a = msg
+    a &= 0xFFFF
+    a >>= 8
 
+    b = int(msg)
+    b &= 0b1000000
+    b = ~b
 
-def main(msg):
-    a = add_zero(bin(int(msg, 0))[2:])
-    first_par = add_zero(hex(int('0b' + a[16:24], 0))[2:])
-    second_par = reverse_bit(a[::-1][6:7])
-    third_par = hex(int('0b' + a[12:16][::-1], 0))[2:]
+    c = int(msg)
+    c &= 0b11110000000000000000
+    c >>= 16
+    c = rev_bits(c)
 
-    print('Второй байт:                   ', first_par.upper())
-    print('Значение 7-го бита:            ', second_par.upper())
-    print('Зеркальное отб. 17-20 го бита: ', third_par.upper())
-    print('Дополнительные параметры:      ', (first_par + second_par + third_par).upper())
+    print('Второй байт:                   ', hex(a), ' ', a)
+    print('Значение 7-го бита:            ', bin(b), ' ', b)
+    print('Зеркальное отб. 17-20 го бита: ',  bin(c), ' ', c)
 
 
 if __name__ == '__main__':
-    main('0x5FABFF01')
+    main2(0x5FABFF01)
